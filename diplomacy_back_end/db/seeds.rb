83c2f22f79965edcb3c76de3740ca7115534d5bc
github username <github email address>
2017-12-04 15:33:28 -0500

    seeded game and user files

--- diplomacy_back_end/db/seeds.rb
+++ diplomacy_back_end/db/seeds.rb
@@ -6,6 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-dick = User.new(username:"diplomacyNbourbon");
-dakota = User.new(username:"diplomacyMaster5000");
-ryan = User.new(username:"rjmascolo")
+dick = User.create(username:"diplomacyNbourbon");
+dakota = User.create(username:"diplomacyMaster5000");
+ryan = User.create(username:"rjmascolo")
+timmy = User.create(username:"timmers")
+ricky = User.create(username:"ricky")
+smith = User.create(username:"mrs smith")
+belle = User.create(username:"belle")
+
+game = Game.create(name: "big battle")
+
+russia = Country.create(name: "Russia" ,game: game , user: ryan)
+austria = Country.create(name: "Austria",game: game , user: dakota)
+england = Country.create(name:"England" ,game_id: game , user: dick)
+france = Country.create(name: "France",game: game , user: timmy)
+germany = Country.create(name: "Germany",game: game , user: ricky)
+italy  = Country.create(name: "Italy",game: game , user: smith)
+turkey = Country.create(name: "Turkey",game: game , user: belle)
