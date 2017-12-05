8cee1e2b1f30e38722899105ab14b33ce2b25c5b
Gonzodamus <Dick@TheDickWard.com>
2017-12-05 14:18:11 -0500

    added CORS into gemfile

---
+++ frontend/js/game.js
@@ -0,0 +1,19 @@
+
+
+// createOrder(turnId, unitId, phase, orderType, fromTerritory, toTerritory, coast) {
+//   let obj = {
+//       headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//       },
+//       method: "post",
+//       body: JSON.stringify({turn_id: turnId,
+//                             unit_id: unitId,
+//                             phase: phase,
+//                             order_type: orderType,
+//                             from_territory: fromTerritory,
+//                             to_territory: toTerritory,
+//                             coast: coast})
+//   }
+//   fetch('http://localhost:3000/orders', obj).then(res => res.json())
+// }
