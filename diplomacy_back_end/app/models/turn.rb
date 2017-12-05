c897ad23c29a32d132e6dec79923f1ae71dcd6b2
Gonzodamus <Dick@TheDickWard.com>
2017-12-05 16:09:41 -0500

    added cors

--- diplomacy_back_end/app/models/turn.rb
+++ diplomacy_back_end/app/models/turn.rb
@@ -1,4 +1,15 @@
 class Turn < ApplicationRecord
   belongs_to :game
   has_many :orders
+
+  def advance_turn
+    if self.season == "Spring"
+      self.season = "Fall"
+    else
+      self.season = "Spring"
+      self.year += 1
+    end
+    "#{self.season}, #{self.year}"
+  end
+
 end
