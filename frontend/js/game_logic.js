6bd449d6c2c4a3e6b9b2a252892b1dab0a612a7d
github username <github email address>
2017-12-06 21:03:41 -0500

    executes non conflicting orders

--- frontend/js/game_logic.js
+++ frontend/js/game_logic.js
@@ -14,7 +14,7 @@ let allUnitsArray = [].concat.apply([], allUnitsNested)
 
 let ordersArray = [new Order(1, "move", countries.Germany.units[0], territories.Ber, territories.Kie ),
                    new Order(1, "move", countries.Germany.units[1], territories.Mun, territories.Bur ),
-                   new Order(1, "move", countries.Germany.units[2], territories.Kie, territories.Ruh ),
+                   new Order(1, "support", countries.Germany.units[2], territories.Mun, territories.Bur ),
                    new Order(1, "move", countries.France.units[0], territories.Par, territories.Bur ),
                    new Order(1, "support", countries.France.units[1], territories.Par, territories.Bur ),
                    new Order(1, "move", countries.France.units[2], territories.Bre, territories.Gas )]
@@ -30,7 +30,7 @@ function isThereConflict(ordersArray){
   let array = []
   let conflictsArray = []
   ordersArray.forEach ( order => {
-      if (order.type === "move" || order.type === "hold"){
+      if (orderTypeHoldOrMove(order)){
         if (array.includes(order.destination)){
           conflictsArray.push(order.destination)
         }
@@ -41,28 +41,67 @@ function isThereConflict(ordersArray){
 }
 
 function orderResolution(ordersArray){
-  let conflicts = isThereConflict(ordersArray)
-  let newOrders;
-      conflicts.forEach( conflict => {
-      newOrders = resolveConflict(ordersArray, conflict)
-  })
-  return newOrders
-  // moveResolution(newOrdersArray)
+  // grab conflicting locations
+  addSupports(ordersArray)
+  let conflictingLocations = isThereConflict(ordersArray)
+  let conflictOrders = conflictingOrders(ordersArray, conflictingLocations)
+  let nonconflictingOrders = nonConflictingOrders(ordersArray, conflictingLocations)
+
+  while (conflictingLocations.length > 0) {
+    let x = resolveConflict(conflictOrders, conflictingLocations[0])
+    if (x != undefined){
+      nonconflictingOrders.push(x)
+    }
+    conflictingLocations.shift()
+  }
+  moveResolution(nonconflictingOrders);
+  return nonconflictingOrders
 }
 
-
-
-function resolveConflict(ordersArray, conflict){
-  var conflictingOrders =  ordersArray.filter(order => {
+function resolveConflict(conflictOrders, conflict){
+  var conflictingOrders =  conflictOrders.filter(order => {
     return order.destination == conflict
   })
-  return conflictingOrders.
+  var maximum = Math.max.apply(Math, conflictingOrders.map(order => order.support));
+  conflictingOrders = conflictingOrders.filter( order => order.support === maximum)
+  if (conflictingOrders.length === 1){
+    return conflictingOrders[0];
+  }
 }
+
+
+function conflictingOrders(ordersArray, conflictingLocations){
+  let conflictOrders = []
+  ordersArray.forEach( order => {
+    conflictingLocations.forEach( location => {
+      if (order.destination == location && orderTypeHoldOrMove(order)){
+
+        conflictOrders.push(order);
+      }
+    })
+  })
+    return conflictOrders
+}
+
+function nonConflictingOrders(ordersArray, conflictingLocations){
+ var newOrders = ordersArray
+ let issues = [...conflictingLocations]
+  while (issues.length > 0 ) {
+      newOrders = newOrders.filter( order => order.destination != conflictingLocations[0])
+      issues.shift()
+  }
+  return newOrders
+}
+
+function orderTypeHoldOrMove(order) {
+  return order.type == "move" || order.type == "hold"
+}
+
 
 const supports = (ordersArray) => {
   return ordersArray.filter(order => {
-  return order.type === "support"
-})
+    return order.type === "support"
+  })
 }
 
 
