099c6c3c191270e60f238b115e551864fafd3b8d
github username <github email address>
2017-12-07 16:00:37 -0500

    push up

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -19,7 +19,10 @@ function play() {
       break;
     case "Order Resolution Phase":
       retreatingUnits = orderResolution(orderStore);
-      debugger;
+      if (retreatingUnits != []) {
+        debugger;
+        retreatingUnits.forEach( issue => alert(retreatingUnits.name))
+      }
       addUnits();
       clearOrderDiplay()
       updateDisplay();
